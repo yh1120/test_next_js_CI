9219d53954d26accfd394eb2719f74452a97d857
Brad <yonghun1120@gmail.com>
2022-12-17 00:45:42 +0900

    change text

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
-          <div>TEST!!!!!!!!!!!!!!!!!!!!!!!!!</div>
+          <div>TEST</div>
           <p>
             Get started by editing&nbsp;
             <code className={styles.code}>pages/index.tsx</code>
